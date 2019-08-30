380dd8bc158f1f2c4aceee89e74f00c4d7ccc15c
kjablonski08 <kjablonski08@gmail.com>
2019-08-30 17:54:50 -0400

    Done.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,6 +48,11 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  class Application < Rails::Application
+    config.web_console.whitelisted_ips = '<IP address here>'
+  end
+  
+
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
